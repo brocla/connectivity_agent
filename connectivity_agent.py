3369f6c5de8627731b5401ca180276b8f1c58dc0
Kevin Brown <git@whidit.com>
2025-11-19 20:39:58 -0700

    restric the Mac lsof command to only look at TCP connections, not UDP. Prevents a Rate limit error from too much output.

--- connectivity_agent.py
+++ connectivity_agent.py
@@ -130,7 +130,7 @@ TOOLS: Dict[str, Any] = {
     "darwin": {
         "ping": partial(run_command, ["ping", "-c", "4"]),
         "curl": partial(run_command, ["curl", "-I"]),
-        "ports": partial(run_command, ["lsof", "-i", "-n", "-P"]),
+        "ports": partial(run_command, ["lsof", "-iTCP", "-n", "-P"]),
         "tracert": partial(run_command, ["traceroute", "-n", "-m", "8", "-w", "2"], timeout=15),
         "nslookup": partial(run_command, ["nslookup"]),
         "ipconfig": partial(run_command, ["ifconfig"]),
